8a9cfc76fbec5d419164dbfb7b2eb4eddf9d5039
Mitaka <dimitar_dimitrov12@mail.bg>
2019-09-10 15:30:06 +0300

    day3 homework

---
+++ day3/PB_Day3_08.09.19/01. USD to BGN/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._USD_to_BGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double valute = double.Parse(Console.ReadLine());
+
+            double convert = valute * 1.79549;
+
+            Console.WriteLine($"{convert:f2}");
+        }
+    }
+}
---
+++ day3/PB_Day3_08.09.19/04. Tailoring Workshop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04._Tailoring_Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numberRectangularTables = int.Parse(Console.ReadLine());
+
+            double tablelenght = double.Parse(Console.ReadLine());
+
+            double tableWidth = double.Parse(Console.ReadLine());
+
+            double areaSheets = numberRectangularTables * (tablelenght + 2 * 0.30) * (tableWidth + 2 * 0.30);
+            double tableClothes = numberRectangularTables * (tablelenght / 2) * (tablelenght / 2);
+
+            double priceInUSD = areaSheets * 7 + tableClothes * 9;
+            double priceInBGN = priceInUSD * 1.85;
+           // Console.WriteLine(areaSheets);
+           // Console.WriteLine(tableClothes);
+            Console.WriteLine($"{priceInUSD:f2} USD");
+            Console.WriteLine($"{priceInBGN:f2} BGN");
+        }
+    }
+}
